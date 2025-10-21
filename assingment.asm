20c8b9e7c1f1e4fb116e3ea95b2d3aea444e2297
zhanjiea <jaychow2004@gmail.com>
2025-10-21 17:57:11 +0800

    update

--- assingment.asm
+++ assingment.asm
@@ -65,14 +65,25 @@ section .data
     choice3 db  'ADD ITEM STOCK', 0xA
             db  'Enter item name: ',
     len_choice3 equ $ - choice3
-    stockValueInput db  'Enter stock to add: ',
-    len_addStockValue equ $ - stockValueInput
+    addStockValueInput db  'Enter stock to add: ',
+    len_addStockValue equ $ - addStockValueInput
 
-    choice4 db  'Reduce item stock', 0xA
+    choice4 db  'REDUCE ITEM STOCK', 0xA
+            db  'Enter item name: ',
     len_choice4 equ $ - choice4
+    reduceStockValueInput db  'Enter stock to reduce: ',
+    len_reduceStockValue equ $ - reduceStockValueInput
 
-    choice5 db  'Delete item', 0xA
+    itemNotFound db 'Item not found!', 0xA
+    len_itemNotFound equ $ - itemNotFound
+    stockUpdated db 'Stock updated successfully!', 0xA
+    len_stockUpdated equ $ - stockUpdated
+
+    choice5 db  'DELETE ITEM', 0xA
+    db  'Enter item name: ',
     len_choice5 equ $ - choice5
+    itemDeleted db 'Item deleted successfully!', 0xA
+    len_itemDeleted equ $ - itemDeleted
 
     undefined db 'Undefined Choice, Please Choose again', 0xA
     len_undefined equ $ - undefined
@@ -82,12 +93,17 @@ section .bss
 menuInput resb 1
 newmItem resb 64
 readFileBuffer resb 128
-editStockFileBuffer resb 1024
-bytesRead resd 1
 addStockItem resb 64
-addStockValue resb 1
-numStartESI resd 1
-tempNum resb 32
+addStockValue resb 10
+fileBuffer resb 2048        ; buffer to hold entire file
+fileBytesRead resd 1        ; total bytes read from file
+outputBuffer resb 2048      ; buffer for modified content
+outputLen resd 1            ; length of output
+itemLen resd 1
+tempNumBuf resb 16
+reduceStockItem resb 64
+reduceStockValue resb 10
+deleteItemname resb 64
 ;-------------------------------------------------------------------------------------------
 section .text
     global _start
@@ -162,8 +178,8 @@ condition3:
 
 .get_stock_value:
     ; Get stock value to add
-    print stockValueInput, len_addStockValue
-    input addStockValue, 1
+    print addStockValueInput, len_addStockValue
+    input addStockValue, 10
 
 .check_stock_value:
     mov al, [addStockValue]
@@ -172,23 +188,124 @@ condition3:
     jmp .update_stock
 
 .ask_stock_value:
-    input addStockValue, 1
+    input addStockValue, 10
     jmp .check_stock_value
 
 .update_stock:
-    ; Call addStock function to update stock value in file
-    call addStock
+    ; Parse the entire input string to a number
+    mov esi, addStockValue
+    xor eax, eax                ; Result accumulator
+    xor ebx, ebx                ; Temp for digit
+    
+.parse_input_loop:
+    mov bl, [esi]
+    cmp bl, 0xA                 ; Check for newline
+    je .done_parse_input
+    cmp bl, 0                   ; Check for null
+    je .done_parse_input
+    cmp bl, '0'                 ; Check if valid digit
+    jb .done_parse_input
+    cmp bl, '9'
+    ja .done_parse_input
+    
+    imul eax, 10                ; eax *= 10
+    sub bl, '0'                 ; Convert ASCII to number
+    add eax, ebx                ; Add digit
+    inc esi
+    jmp .parse_input_loop
+    
+.done_parse_input:
+    push eax                    ; Push the NUMERIC value (not character)
+    push addStockItem           
+    call editStock
+    add esp, 8
 
     print newline, 1
     jmp print_menu
 
 condition4:
     print choice4, len_choice4
+    input reduceStockItem, 64
+
+.check_item_name:
+    mov al, [reduceStockItem]
+    cmp al, 0xA
+    je .ask_item_name
+    jmp .get_stock_value
+
+.ask_item_name:
+    input reduceStockItem, 64
+    jmp .check_item_name
+
+.get_stock_value:
+    ; Get stock value to add
+    print reduceStockValueInput, len_reduceStockValue
+    input reduceStockValue, 10
+
+.check_stock_value:
+    mov al, [reduceStockValue]
+    cmp al, 0xA
+    je .ask_stock_value
+    jmp .update_stock
+
+.ask_stock_value:
+    input reduceStockValue, 10
+    jmp .check_stock_value
+
+.update_stock:
+    ; Parse the entire input string to a number
+    mov esi, reduceStockValue
+    xor eax, eax                ; Result accumulator
+    xor ebx, ebx                ; Temp for digit
+    
+.parse_input_loop:
+    mov bl, [esi]
+    cmp bl, 0xA                 ; Check for newline
+    je .done_parse_input
+    cmp bl, 0                   ; Check for null
+    je .done_parse_input
+    cmp bl, '0'                 ; Check if valid digit
+    jb .done_parse_input
+    cmp bl, '9'
+    ja .done_parse_input
+    
+    imul eax, 10                ; eax *= 10
+    sub bl, '0'                 ; Convert ASCII to number
+    add eax, ebx                ; Add digit
+    inc esi
+    jmp .parse_input_loop
+    
+.done_parse_input:
+    push eax                    ; Push the NUMERIC value (not character)
+    push reduceStockItem           
+    call editStock
+    add esp, 8
+
     print newline, 1
     jmp print_menu
 
 condition5:
     print choice5, len_choice5
+    input deleteItemname, 64
+
+.check_item_name:
+    mov al, [deleteItemname]
+    cmp al, 0xA
+    je .ask_item_name
+    jmp .do_delete
+
+.ask_item_name:
+    input deleteItemname, 64
+    jmp .check_item_name
+
+.do_delete:
+    ; Call deleteItem
+    xor eax, eax               ; No numeric value needed
+    push eax
+    push deleteItemname
+    call editStock
+    add esp, 8
+
     print newline, 1
     jmp print_menu
 
@@ -293,172 +410,256 @@ createItem:
     ret
 
 ;-------------------------------------------------------------------------------------------
-scan_buffer:
-    mov esi, editStockFileBuffer    ; start of buffer
-
-.next_char:
-    mov al, [esi]                   ; get current file byte
-    cmp al, 0                       ; end of file buffer?
-    je .not_found                   ; stop if reached end
-    cmp al, [edi]                   ; first character matches first of input?
-    jne .skip                       ; if not, continue searching
-
-    ; now check the rest of the string
-    push esi                        ; pass current file position
-    push edi                        ; pass pointer to search term (input)
-    call compare_strings
-    add esp, 8                      ; clean up (we pushed esi, edi)
-    cmp eax, 1                      ; if eax == 1, full match
-    je .found                       ; match found 
-
-.skip:
-    inc esi                         ; move to next file byte
-    jmp .next_char
-
-.not_found:
-    ; not found case
-    jmp .done
-
-.found:
-
-.find_comma:
-    mov al, [esi]
-    cmp al, ','
-    je .after_comma
-    inc esi
-    jmp .find_comma
-
-.after_comma:
-    inc esi       ; now ESI points to first digit ('1' in "10")
-    mov [numStartESI], esi  ; save position of number start
-    print numStartESI, 1
-
-.done:
-    ret
-
-; Compare strings (ESI = position in file, EDI =  (input) pointer to search term) byte-by-byte
-compare_strings:
-.loop:
-    mov al, [esi]
-    mov bl, [edi]
-    cmp bl, 0           ; end of search string?
-    je .match           ; reached end => full match
-    cmp al, bl
-    jne .no_match
-    inc esi             ; move to next char in file
-    inc edi             ; move to next char in search term
-    jmp .loop
-
-.match:
-    mov eax, 1          ; eax = 1 == match found
-    ret
-
-.no_match:
-    xor eax, eax        ; eax = 0 == no match
-    ret
-
-; Convert ASCII string of digits at ESI to integer in EAX
-ascii_to_int:
-    xor eax, eax
-.convert_loop:
-    mov bl, [esi]
-    cmp bl, '0'
-    jb .done         ; if < '0' not digit
-    cmp bl, '9'
-    ja .done         ; if > '9' not digit
-    sub bl, '0'
-    and ebx, 0xFF
-    imul eax, 10
-    add eax, ebx
-    inc esi
-    jmp .convert_loop
-.done:
-    ret
-
-; Convert integer in EAX to ASCII string at address in EAX
-int_to_ascii:
-    mov ecx, 0
-
-.reverse_loop:
-    mov edx, 0
-    mov ebx, 10
-    div ebx              ; divide EAX by 10, remainder in EDX
-    add dl, '0'
-    push dx
-    inc ecx
-    test eax, eax
-    jnz .reverse_loop
-
-.write_loop:
-    pop dx
-    mov [edi], dl
-    inc edi
-    loop .write_loop
-    mov byte [edi], 0
-    ret
-
-;----------------------------------------------------------------
-openFileForEdit:
+editStock:
     push ebp
     mov ebp, esp
-
-    ; ===== Open file for reading =====
+    push ebx
+    push esi
+    push edi
+    
+    ; Calculate item name length (without newline)
+    mov esi, [ebp+8]
+    xor ecx, ecx
+.find_len:
+    mov al, [esi + ecx]
+    cmp al, 0xA
+    je .found_len
+    cmp al, 0
+    je .found_len
+    inc ecx
+    jmp .find_len
+.found_len:
+    mov [itemLen], ecx
+    
+    ; Open and read file
     openfile filename, 0, 0
     cmp eax, 0
-    js .file_error          ; if < 0, jump (file not found or error)
-    mov esi, eax            ; save file descriptor
-
-    ; ===== Read the whole file and saved it into editStockFileBuffer =====
-    mov eax, 3              ; sys_read
-    mov ebx, esi
-    mov ecx, editStockFileBuffer
-    mov edx, 1024
+    js .file_error
+    mov ebx, eax
+    
+    mov eax, 3
+    mov ecx, fileBuffer
+    mov edx, 2048
     int 0x80
-    mov [bytesRead], eax
+    
+    mov [fileBytesRead], eax
+    
+    mov eax, 6
+    int 0x80
+    
+    cmp dword [fileBytesRead], 0
+    jle .file_error
+    
+    ; Parse file
+    xor esi, esi                ; input pos
+    xor edi, edi                ; output pos
+    
+.line_loop:
+    cmp esi, [fileBytesRead]
+    jge .item_not_found
+    
+    mov ebx, esi                ; save line start
+    
+    ; Compare item name
+    mov edx, [ebp+8]
+    xor ecx, ecx
+.cmp_loop:
+    cmp ecx, [itemLen]
+    je .check_comma
+    
+    mov al, [fileBuffer + esi]
+    cmp al, [edx + ecx]
+    jne .no_match
+    
+    inc ecx
+    inc esi
+    jmp .cmp_loop
+    
+.check_comma:
+    mov al, [fileBuffer + esi]
+    cmp al, ','
+    je .found_match
+    
+.no_match:
+    mov esi, ebx
+.copy_line:
+    mov al, [fileBuffer + esi]
+    mov [outputBuffer + edi], al
+    inc esi
+    inc edi
+    cmp al, 0xA
+    jne .copy_line
+    jmp .line_loop
+    
+.found_match:
+    ;compare to see whether it is delete
+    movzx edx, byte [menuInput]  ;  Use ebx instead of al
+    cmp dl, '5'                  ;  Compare lower byte
+    je .delete_item
 
-    ; ===== Close file =====
-    closefile esi
+    ; Copy name and comma
+    mov esi, ebx
+    mov ecx, [itemLen]
 
-    mov esp, ebp
-    pop ebp
-    ret
+.copy_name:
+    mov al, [fileBuffer + esi]
+    mov [outputBuffer + edi], al
+    inc esi
+    inc edi
+    loop .copy_name
+    
+    ; Copy comma and optional space
+    mov al, [fileBuffer + esi]
+    mov [outputBuffer + edi], al
+    inc esi
+    inc edi
+    
+    mov al, [fileBuffer + esi]
+    cmp al, ' '
+    jne .parse_num
+    mov [outputBuffer + edi], al
+    inc esi
+    inc edi
+    
+.parse_num:
+    ; Parse number
+    xor eax, eax
 
+.parse_loop:
+    movzx ebx, byte [fileBuffer + esi]
+    cmp bl, '0'
+    jb .done_parse
+    cmp bl, '9'
+    ja .done_parse
+    
+    imul eax, 10
+    sub bl, '0'
+    add eax, ebx
+    inc esi
+    jmp .parse_loop
+    
+.done_parse:
+    movzx ebx, byte [menuInput]  ;  Use ebx instead of al
+    cmp bl, '3'                  ;  Compare lower byte
+    je .add_stock
+    cmp bl, '4'
+    je .reduce_stock
+    jmp .cleanup
+
+.add_stock:
+    ; Add new stock (already a number on stack)
+    mov ebx, [ebp+12]         ; Get numeric value directly
+    add eax, ebx              ; Add it (no conversion needed!)
+    
+    ; Convert to string using tempNumBuf
+    mov ebx, 10
+    lea ecx, [tempNumBuf + 15]  ; point to end
+    mov byte [ecx], 0           ; null terminator
+    jmp .to_string
+
+.reduce_stock:
+    ; Reduce new stock (already a number on stack)
+    mov ebx, [ebp+12]         ; Get numeric value directly
+    sub eax, ebx              ; Add it (no conversion needed!)
+    
+    ; Convert to string using tempNumBuf
+    mov ebx, 10
+    lea ecx, [tempNumBuf + 15]  ; point to end
+    mov byte [ecx], 0           ; null terminator
+    jmp .to_string
+
+.to_string:
+    dec ecx
+    xor edx, edx
+    div ebx
+    add dl, '0'
+    mov [ecx], dl
+    test eax, eax
+    jnz .to_string
+    
+    ; Copy number to output
+.copy_number:
+    mov al, [ecx]
+    test al, al
+    jz .done_number
+    mov [outputBuffer + edi], al
+    inc ecx
+    inc edi
+    jmp .copy_number
+    
+.done_number:
+    ; Copy rest of current line
+.copy_eol:
+    cmp esi, [fileBytesRead]
+    jge .copy_remaining
+    mov al, [fileBuffer + esi]
+    mov [outputBuffer + edi], al
+    inc esi
+    inc edi
+    cmp al, 0xA
+    jne .copy_eol
+    jmp .copy_remaining
+
+.delete_item:
+    ; Skip rest of current line
+    mov esi, ebx  
+    
+.skip_line:
+    cmp esi, [fileBytesRead]
+    jge .copy_remaining
+    mov al, [fileBuffer + esi]
+    inc esi
+    cmp al, 0xA                 ; found newline?
+    jne .skip_line
+    ; Line skipped, continue with next line
+    jmp .copy_remaining
+    
+.copy_remaining:
+    ; Copy remaining lines
+    cmp esi, [fileBytesRead]
+    jge .write_file
+    mov al, [fileBuffer + esi]
+    mov [outputBuffer + edi], al
+    inc esi
+    inc edi
+    jmp .copy_remaining
+    
+.write_file:
+    ; Write back to file
+    openfile filename, 0x241, 0o644
+    cmp eax, 0
+    js .file_error
+    mov ebx, eax
+    
+    mov eax, 4
+    mov ecx, outputBuffer
+    mov edx, edi                ; use edi as length
+    int 0x80
+    
+    mov eax, 6
+    int 0x80
+    
+    ; Check what operation was done for appropriate message
+    movzx eax, byte [menuInput]
+    cmp al, '5'
+    je .show_deleted
+    print stockUpdated, len_stockUpdated
+    jmp .cleanup
+
+.show_deleted:
+    print itemDeleted, len_itemDeleted
+    jmp .cleanup
+    
+.item_not_found:
+    print itemNotFound, len_itemNotFound
+    jmp .cleanup
+    
 .file_error:
     print errorMsg, len_errorMsg
-
-addStock:
-    push ebp
-    mov ebp, esp
-    ; ===== Open file and read into buffer =====
-    call openFileForEdit
-    ; ===== Scan for item in buffer =====
-    mov edi, addStockItem   ; pointer to input item name
-    call scan_buffer
-
-    ; Convert to integer and add the value
-    call ascii_to_int           ; convert ASCII digits at ESI to integer in EAX
-    movzx ebx, byte [addStockValue] ; get ASCII digit
-    sub ebx, '0'                    ; convert to real integer (e.g. '1' -> 1)
-    add eax, ebx                    ; now add it properly
-
-    ; Convert back to ASCII and write it back to buffer
-    mov edi, [numStartESI]      ; destination to write new digits
-    call int_to_ascii           ; convert new value -> ASCII digits
-
-    ; ===== Open file for writing (overwrite) =====
-    openfile filename, 0x201, 0o644
-    mov esi, eax                ; save file descriptor
-
-    ; ===== Write updated buffer back to file =====
-    mov eax, 4                      ; sys_write
-    mov ebx, esi
-    mov ecx, editStockFileBuffer
-    mov edx, [bytesRead]             ; number of bytes read (saved value)
-    int 0x80
-
-    ; ===== Close file =====
-    closefile esi
-
-    mov esp, ebp
+    
+.cleanup:
+    pop edi
+    pop esi
+    pop ebx
     pop ebp
     ret
